656aa6341895352c9c931da02e8fec7ea437889c
Isaque Silva <isaque.silva@equilibriumweb.com>
2023-03-27 16:46:34 -0300

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,23 +4,48 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\{
+    Hash,
+    Auth,
+    Cookie
+};
+use Symfony\Component\HttpFoundation\Response as HttpFoundationResponse;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $user = User::create([
+        return User::create([
             'name'=> $request->input('name'),
             'email'=> $request->input('email'),
             'password'=> Hash::make($request->input('password'))
         ]);
-
-        return $user;
     }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password')))
+        {
+            return response([
+                'massage' => 'Invalid crendentials',
+
+            ], HttpFoundationResponse::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $token, 60 * 24); // 1 day
+
+        return response([
+            'message' => $token
+        ])->withCookie($cookie);
+    }
+
     public function user()
     {
-        return "Authentication user";
+        return Auth::user();
 
     }
 }
